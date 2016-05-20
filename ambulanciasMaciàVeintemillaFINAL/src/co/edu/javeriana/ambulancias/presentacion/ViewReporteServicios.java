35a8233f3797c2adb787acd4016d17070dfd3d7d
Juan Pablo Peñaloza Botero <juan-penaloza@javeriana.edu.co>
2016-05-20 10:58:45 -0500

    Add ViewReporteServicios

--- ambulanciasMaciàVeintemillaFINAL/src/co/edu/javeriana/ambulancias/presentacion/ViewReporteServicios.java
+++ ambulanciasMaciàVeintemillaFINAL/src/co/edu/javeriana/ambulancias/presentacion/ViewReporteServicios.java
@@ -2,13 +2,100 @@ package co.edu.javeriana.ambulancias.presentacion;
 
 import javax.swing.JPanel;
 
+import Controller.Controller;
+
+import java.awt.BorderLayout;
+
 public class ViewReporteServicios extends JPanel {
 
-	/**
-	 * Create the panel.
-	 */
-	public ViewReporteServicios() {
+	private static final long serialVersionUID = 1L;
+	
+	private TestGUIAmbulancias ventanaPrincipal;
+	private Controller controller;
+	
+	private BorderLayout layoutPrincipal;
+	private JPanel panelNorth;
+	private JPanel panelEast;
+	private JPanel panelWest;
+	private JPanel panelSouth;
+	private JPanel panelCenter;
+	
+
+	public ViewReporteServicios(TestGUIAmbulancias ventanaPrincipal) {
+		
+		this.ventanaPrincipal= ventanaPrincipal;
+		this.controller = new Controller (this.ventanaPrincipal);
+		
+		layoutPrincipal = new BorderLayout();
+		setLayout(layoutPrincipal);
+		
+		JPanel panelNorth = new JPanel();
+		add(panelNorth, BorderLayout.NORTH);
+		
+		JPanel panelEast = new JPanel();
+		add(panelEast, BorderLayout.WEST);
+		
+		JPanel panelWest = new JPanel();
+		add(panelWest, BorderLayout.EAST);
+		
+		JPanel panelSouth = new JPanel();
+		add(panelSouth, BorderLayout.SOUTH);
+		
+		JPanel panelCenter = new JPanel();
+		add(panelCenter, BorderLayout.CENTER);
 
 	}
 
+	public BorderLayout getLayoutPrincipal() {
+		return layoutPrincipal;
+	}
+
+	public void setLayoutPrincipal(BorderLayout layoutPrincipal) {
+		this.layoutPrincipal = layoutPrincipal;
+	}
+
+	public JPanel getPanelNorth() {
+		return panelNorth;
+	}
+
+	public void setPanelNorth(JPanel panelNorth) {
+		this.panelNorth = panelNorth;
+	}
+
+	public JPanel getPanelEast() {
+		return panelEast;
+	}
+
+	public void setPanelEast(JPanel panelEast) {
+		this.panelEast = panelEast;
+	}
+
+	public JPanel getPanelWest() {
+		return panelWest;
+	}
+
+	public void setPanelWest(JPanel panelWest) {
+		this.panelWest = panelWest;
+	}
+
+	public JPanel getPanelSouth() {
+		return panelSouth;
+	}
+
+	public void setPanelSouth(JPanel panelSouth) {
+		this.panelSouth = panelSouth;
+	}
+
+	public JPanel getPanelCenter() {
+		return panelCenter;
+	}
+
+	public void setPanelCenter(JPanel panelCenter) {
+		this.panelCenter = panelCenter;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
 }
