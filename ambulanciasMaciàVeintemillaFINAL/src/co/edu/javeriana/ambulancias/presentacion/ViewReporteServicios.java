2816a8772b60e2f1b7d41002e0e7e9d83d99b066
Juan Pablo Peñaloza Botero <juan-penaloza@javeriana.edu.co>
2016-05-20 12:29:48 -0500

    Added View Servicios con ips

--- ambulanciasMaciàVeintemillaFINAL/src/co/edu/javeriana/ambulancias/presentacion/ViewReporteServicios.java
+++ ambulanciasMaciàVeintemillaFINAL/src/co/edu/javeriana/ambulancias/presentacion/ViewReporteServicios.java
@@ -1,10 +1,18 @@
 package co.edu.javeriana.ambulancias.presentacion;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 
 import Controller.Controller;
-
-import java.awt.BorderLayout;
+import net.miginfocom.swing.MigLayout;
 
 public class ViewReporteServicios extends JPanel {
 
@@ -13,6 +21,8 @@ public class ViewReporteServicios extends JPanel {
 	private TestGUIAmbulancias ventanaPrincipal;
 	private Controller controller;
 	
+	private JButton btnMostrar;
+	
 	private BorderLayout layoutPrincipal;
 	private JPanel panelNorth;
 	private JPanel panelEast;
@@ -20,6 +30,22 @@ public class ViewReporteServicios extends JPanel {
 	private JPanel panelSouth;
 	private JPanel panelCenter;
 	
+	private JTable tableServicios;
+	private JTable tableIPS;
+	private JTable tableAmbulancias;
+	
+	private TableModel modelServicios;
+	private TableModel modelIPS;
+	private TableModel modelAmbulancia;
+	
+	private String [] tableLabelsAmbulancia = {"Codigo","Tipo","Tipo UCI","Placa", "Medico / Enfermero","Hora Posicion", "Calle", "Carrera", "Tarifa"};
+	private String [][] tableContAmbulancias = {};
+	
+	private String [] tableLabelsIPS = {"Nombre" , "Tipo Atencion" , "Direccion"};
+	private String [][] tableContIPS = {};
+	
+	private String [] tableLabelsServicios = {"Codigo","Hora","Paciente", "Tipo Servicio","Telefono","Direccion","Estado","IPS","Ambulancia","Valor"};
+	private String [][] tableContServicios = {};
 
 	public ViewReporteServicios(TestGUIAmbulancias ventanaPrincipal) {
 		
@@ -29,21 +55,77 @@ public class ViewReporteServicios extends JPanel {
 		layoutPrincipal = new BorderLayout();
 		setLayout(layoutPrincipal);
 		
-		JPanel panelNorth = new JPanel();
+		panelNorth = new JPanel();
 		add(panelNorth, BorderLayout.NORTH);
 		
-		JPanel panelEast = new JPanel();
+		panelEast = new JPanel();
 		add(panelEast, BorderLayout.WEST);
 		
-		JPanel panelWest = new JPanel();
+		panelWest = new JPanel();
 		add(panelWest, BorderLayout.EAST);
 		
-		JPanel panelSouth = new JPanel();
+		panelSouth = new JPanel();
 		add(panelSouth, BorderLayout.SOUTH);
 		
-		JPanel panelCenter = new JPanel();
+		panelCenter = new JPanel();
+		
+		setTittleView();
+		setTableServiciosView ();
+		setBtnMostrarView ();
+		setTableIPSView ();
+		setTableAmbulanciaView ();
+		
 		add(panelCenter, BorderLayout.CENTER);
+		
+		
 
+	}
+	
+	public void setTableServiciosView () {
+		
+		JLabel tittle = new JLabel("Servicios");
+		panelCenter.add(tittle, "cell 0 1,grow");
+		
+		modelServicios = new DefaultTableModel(tableContServicios, tableLabelsServicios);
+		tableServicios = new JTable(modelServicios);
+		JScrollPane scrollPane = new JScrollPane(tableServicios);
+		scrollPane.setBounds(0, 128, 345, -128);
+		panelCenter.add(scrollPane, "cell 0 2,grow");
+	}
+	
+	public void setTableIPSView () {
+		JLabel tittle = new JLabel("IPS");
+		panelCenter.add(tittle, "cell 0 4,grow");
+		
+		modelIPS = new DefaultTableModel(tableContIPS, tableLabelsIPS);
+		tableIPS = new JTable(modelIPS);
+		JScrollPane scrollPane = new JScrollPane(tableIPS);
+		scrollPane.setBounds(0, 128, 345, -128);
+		panelCenter.add(scrollPane, "cell 0 5,grow");
+	}
+	
+	public void setTableAmbulanciaView () {
+		JLabel tittle = new JLabel("Ambulancias");
+		panelCenter.add(tittle, "cell 0 6,grow");
+		
+		modelAmbulancia = new DefaultTableModel(tableContAmbulancias, tableLabelsAmbulancia);
+		tableAmbulancias = new JTable(modelAmbulancia);
+		JScrollPane scrollPane = new JScrollPane(tableAmbulancias);
+		scrollPane.setBounds(0, 128, 345, -128);
+		panelCenter.add(scrollPane, "cell 0 7,grow");
+	}
+	
+	public void setTittleView () {
+		panelCenter.setLayout(new MigLayout("", "[562px]", "[27px][27px][27px][27px][27px][27px][27px][27px]"));
+		JLabel tittle = new JLabel("Reporte De Servicios con IPS y Ambulancias Asignados");
+		panelCenter.add(tittle, "cell 0 0,grow");
+	}
+	
+	public void setBtnMostrarView () {
+		btnMostrar = new JButton("Mostrar");
+		
+		panelCenter.add(btnMostrar, "cell 0 3,grow");
+		
 	}
 
 	public BorderLayout getLayoutPrincipal() {
@@ -97,5 +179,31 @@ public class ViewReporteServicios extends JPanel {
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
+
+	public TestGUIAmbulancias getVentanaPrincipal() {
+		return ventanaPrincipal;
+	}
+
+	public void setVentanaPrincipal(TestGUIAmbulancias ventanaPrincipal) {
+		this.ventanaPrincipal = ventanaPrincipal;
+	}
+
+	public Controller getController() {
+		return controller;
+	}
+
+	public void setController(Controller controller) {
+		this.controller = controller;
+	}
+
+	/*public GridLayout getGrid() {
+		return grid;
+	}
+
+	public void setGrid(GridLayout grid) {
+		this.grid = grid;
+	}*/
+	
+	
 	
 }
