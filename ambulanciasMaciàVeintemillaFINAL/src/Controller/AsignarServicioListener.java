c48d096b8e4c2640cd183be0a8d9a21abfa35079
xpablo098 <p_veintemilla@javeriana.edu.co>
2016-05-20 13:44:55 -0500

    Asignar Servico changed

--- ambulanciasMaciàVeintemillaFINAL/src/Controller/AsignarServicioListener.java
+++ ambulanciasMaciàVeintemillaFINAL/src/Controller/AsignarServicioListener.java
@@ -1,5 +1,28 @@
 package Controller;
 
-public class AsignarServicioListener {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.table.DefaultTableModel;
+
+import co.edu.javeriana.ambulancias.negocio.EmpresaAmbulancias;
+import co.edu.javeriana.ambulancias.presentacion.TestGUIAmbulancias;
+
+public class AsignarServicioListener implements ActionListener{
+	
+	TestGUIAmbulancias ventanaPrincipal;
+	
+	public AsignarServicioListener(TestGUIAmbulancias ventanaPrincipal){
+		this.ventanaPrincipal = ventanaPrincipal;
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		ventanaPrincipal.getViewServicios().getTabbedPane().setSelectedIndex(2);
+		
+	}	
+	public void cargarTablas(){
+		String m[][] ;
+		EmpresaAmbulancias ea = (EmpresaAmbulancias)ventanaPrincipal.getEmpresaAmbulancias();
+		ventanaPrincipal.getViewServicios().getViewAsignarServicio().getTablaIPS().setModel((new DefaultTableModel((ea.getModelIPS()), (ventanaPrincipal.getViewServicios().getViewAsignarServicio().getLablesIPS()))));
+	}
 }
--- ambulanciasMaciàVeintemillaFINAL/src/co/edu/javeriana/ambulancias/presentacion/ViewAsignarServicio.java
+++ ambulanciasMaciàVeintemillaFINAL/src/co/edu/javeriana/ambulancias/presentacion/ViewAsignarServicio.java
@@ -48,6 +48,222 @@ public class ViewAsignarServicio extends JPanel {
 	private JLabel lblServicios;
 	private JScrollPane scrollPane_2;
 	
+	public TestGUIAmbulancias getVentanaPrincipal() {
+		return ventanaPrincipal;
+	}
+
+	public void setVentanaPrincipal(TestGUIAmbulancias ventanaPrincipal) {
+		this.ventanaPrincipal = ventanaPrincipal;
+	}
+
+	public Controller getController() {
+		return controller;
+	}
+
+	public void setController(Controller controller) {
+		this.controller = controller;
+	}
+
+	public JTable getTablaIPS() {
+		return tablaIPS;
+	}
+
+	public void setTablaIPS(JTable tablaIPS) {
+		this.tablaIPS = tablaIPS;
+	}
+
+	public DefaultTableModel getModelIps() {
+		return modelIps;
+	}
+
+	public void setModelIps(DefaultTableModel modelIps) {
+		this.modelIps = modelIps;
+	}
+
+	public String[] getLablesIPS() {
+		return lablesIPS;
+	}
+
+	public void setLablesIPS(String[] lablesIPS) {
+		this.lablesIPS = lablesIPS;
+	}
+
+	public String[][] getContIPS() {
+		return contIPS;
+	}
+
+	public void setContIPS(String[][] contIPS) {
+		this.contIPS = contIPS;
+	}
+
+	public JTable getTablaAmb() {
+		return tablaAmb;
+	}
+
+	public void setTablaAmb(JTable tablaAmb) {
+		this.tablaAmb = tablaAmb;
+	}
+
+	public DefaultTableModel getModelAmb() {
+		return modelAmb;
+	}
+
+	public void setModelAmb(DefaultTableModel modelAmb) {
+		this.modelAmb = modelAmb;
+	}
+
+	public String[] getLablesAmbulancias() {
+		return lablesAmbulancias;
+	}
+
+	public void setLablesAmbulancias(String[] lablesAmbulancias) {
+		this.lablesAmbulancias = lablesAmbulancias;
+	}
+
+	public String[][] getContAmbulancias() {
+		return contAmbulancias;
+	}
+
+	public void setContAmbulancias(String[][] contAmbulancias) {
+		this.contAmbulancias = contAmbulancias;
+	}
+
+	public JTable getTablaServ() {
+		return tablaServ;
+	}
+
+	public void setTablaServ(JTable tablaServ) {
+		this.tablaServ = tablaServ;
+	}
+
+	public DefaultTableModel getModelServ() {
+		return modelServ;
+	}
+
+	public void setModelServ(DefaultTableModel modelServ) {
+		this.modelServ = modelServ;
+	}
+
+	public String[] getLablesServ() {
+		return lablesServ;
+	}
+
+	public void setLablesServ(String[] lablesServ) {
+		this.lablesServ = lablesServ;
+	}
+
+	public String[][] getContServ() {
+		return contServ;
+	}
+
+	public void setContServ(String[][] contServ) {
+		this.contServ = contServ;
+	}
+
+	public JPanel getPanel() {
+		return panel;
+	}
+
+	public void setPanel(JPanel panel) {
+		this.panel = panel;
+	}
+
+	public JPanel getPanel_1() {
+		return panel_1;
+	}
+
+	public void setPanel_1(JPanel panel_1) {
+		this.panel_1 = panel_1;
+	}
+
+	public JLabel getLblAsignarUnServicio() {
+		return lblAsignarUnServicio;
+	}
+
+	public void setLblAsignarUnServicio(JLabel lblAsignarUnServicio) {
+		this.lblAsignarUnServicio = lblAsignarUnServicio;
+	}
+
+	public JPanel getPanel_2() {
+		return panel_2;
+	}
+
+	public void setPanel_2(JPanel panel_2) {
+		this.panel_2 = panel_2;
+	}
+
+	public JPanel getPanel_3() {
+		return panel_3;
+	}
+
+	public void setPanel_3(JPanel panel_3) {
+		this.panel_3 = panel_3;
+	}
+
+	public JButton getBtnAceptar() {
+		return btnAceptar;
+	}
+
+	public void setBtnAceptar(JButton btnAceptar) {
+		this.btnAceptar = btnAceptar;
+	}
+
+	public JPanel getPanel_4() {
+		return panel_4;
+	}
+
+	public void setPanel_4(JPanel panel_4) {
+		this.panel_4 = panel_4;
+	}
+
+	public JLabel getLblIps() {
+		return lblIps;
+	}
+
+	public void setLblIps(JLabel lblIps) {
+		this.lblIps = lblIps;
+	}
+
+	public JScrollPane getScrollPane() {
+		return scrollPane;
+	}
+
+	public void setScrollPane(JScrollPane scrollPane) {
+		this.scrollPane = scrollPane;
+	}
+
+	public JLabel getLblAmbulancias() {
+		return lblAmbulancias;
+	}
+
+	public void setLblAmbulancias(JLabel lblAmbulancias) {
+		this.lblAmbulancias = lblAmbulancias;
+	}
+
+	public JScrollPane getScrollPane_1() {
+		return scrollPane_1;
+	}
+
+	public void setScrollPane_1(JScrollPane scrollPane_1) {
+		this.scrollPane_1 = scrollPane_1;
+	}
+
+	public JLabel getLblServicios() {
+		return lblServicios;
+	}
+
+	public void setLblServicios(JLabel lblServicios) {
+		this.lblServicios = lblServicios;
+	}
+
+	public JScrollPane getScrollPane_2() {
+		return scrollPane_2;
+	}
+
+	public void setScrollPane_2(JScrollPane scrollPane_2) {
+		this.scrollPane_2 = scrollPane_2;
+	}
+
 	public ViewAsignarServicio(TestGUIAmbulancias ventanaPrincipal){
 		
 		setLayout(new BorderLayout(0, 0));
