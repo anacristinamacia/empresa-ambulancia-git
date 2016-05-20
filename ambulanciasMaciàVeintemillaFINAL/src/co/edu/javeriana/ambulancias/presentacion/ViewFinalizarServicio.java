7f743a3ce98b1323fd4d564dcbc4e0e914a9c305
tmp00076 <AnaCristina@Anas-MacBook-Pro-3.local>
2016-05-20 11:30:42 -0500

    Arreglado ViewFinalizar

--- ambulanciasMaciàVeintemillaFINAL/src/co/edu/javeriana/ambulancias/presentacion/ViewFinalizarServicio.java
+++ ambulanciasMaciàVeintemillaFINAL/src/co/edu/javeriana/ambulancias/presentacion/ViewFinalizarServicio.java
@@ -2,13 +2,21 @@ package co.edu.javeriana.ambulancias.presentacion;
 
 import javax.swing.JPanel;
 
+import Controller.Controller;
+
 public class ViewFinalizarServicio extends JPanel {
 
+	private TestGUIAmbulancias ventanaPrincipal;
+	private Controller controller;
+	
 	/**
 	 * Create the panel.
 	 */
-	public ViewFinalizarServicio() {
+	public ViewFinalizarServicio(TestGUIAmbulancias ventanaPrincipal) {
 
+		this.ventanaPrincipal = ventanaPrincipal;
+		this.controller = new Controller(this.ventanaPrincipal);
+		
 	}
 
 }
